29dbf8903948b80740201e113a54ab033082e986
bestbinaryboi <101668704+bestbinaryboi@users.noreply.github.com>
2022-03-17 15:18:42 -0400

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,6 +1,19 @@
-building.jpg
-customcarsmain.py
-customcarsthirdpersoncontroller.py
-engine.wav
-tinker.obj
-version.txt
+import requests
+f = open('building.jpg','wb')
+f.write(requests.get('https://raw.githubusercontent.com/bestbinaryboi/customcars/main/building.jpg').content)
+f.close()
+f = open('engine.wav','wb')
+f.write(requests.get('https://raw.githubusercontent.com/bestbinaryboi/customcars/main/engine.wav').content)
+f.close()
+f = open('version.txt','w')
+f.write(requests.get('https://raw.githubusercontent.com/bestbinaryboi/customcars/main/version.txt').text)
+f.close()
+f = open('customcarsmain.py','w')
+f.write(requests.get('https://raw.githubusercontent.com/bestbinaryboi/customcars/main/customcarsmain.py').text)
+f.close()
+f = open('customcarsthirdpersoncontroller.py','w')
+f.write(requests.get('https://raw.githubusercontent.com/bestbinaryboi/customcars/main/customcarsthirdpersoncontroller.py').text)
+f.close()
+f = open('tinker.obj','w')
+f.write(requests.get('https://raw.githubusercontent.com/bestbinaryboi/customcars/main/tinker.obj').text)
+f.close()
