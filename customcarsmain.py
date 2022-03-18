f3864d30ff320f773c53d0472e6fa88a62d9548b
bestbinaryboi <101668704+bestbinaryboi@users.noreply.github.com>
2022-03-18 16:11:40 -0400

    Update customcarsmain.py

--- customcarsmain.py
+++ customcarsmain.py
@@ -1,9 +1,8 @@
 from random import gammavariate
 from ursina import *
-bullet = Entity(model='cube', scale=.1, color=color.black,x=-15,z=-15,collider='box')
 from customcarsthirdpersoncontroller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
-
+    
 app = Ursina()
 window.title="game"
 window.borderless=False
@@ -28,18 +27,28 @@ for i in range(16):
         scale_y = random.uniform(2,3),
         color=color.hsv(0, 0, random.uniform(.9, 1))
     )
-def update():
-    if player.intersects(a).hit:
-           customcarsthirdpersoncontroller.carspeed=customcarsthirdpersoncontroller.carspeed*-1
-           print('player is inside trigger box')
-    else:
-           bullet.color = color.gray
 
 sun = DirectionalLight()
 sun.look_at(Vec3(1,-1,-1))
 Sky()
-bullet = Entity(model='cube', scale=.1, color=color.black,x=-15,z=-15)
 def input(key):
     if key == 'q':
         exit()
 app.run()
+if ss==2:
+    app2 = Ursina()
+    window.title="player2"
+    window.borderless=False
+    player2= FirstPersonController(model="spacething/tinker", color=color.orange, origin_y=-0.5,scale=0.01)
+
+    # the default camera_pivot is (0,2,0)
+    player2.camera_pivot.z = -7.5  # move the camera behind the player model
+    player2.camera_pivot.y = 20.5  # move the camera a little higher
+
+    # setting collider and making it visible for debugging
+    player2.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
+    player2.collider.visible=True
+    def input(key):
+        if key == 'q':
+            exit()
+    app2.run()
